c0471fd5465782b204b1b998e054432784e1ff6a
Yunero <86771168+galliacode@users.noreply.github.com>
2024-04-28 03:19:27 -0400

    Update openai.js

--- functions/libs/openai.js
+++ functions/libs/openai.js
@@ -1,8 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  // organization: "org-0QtdhNE84DCwU7ba3ewW0cN1",
+  organization: "org-0QtdhNE84DCwU7ba3ewW0cN1",
   apiKey: "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"
+  //"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"
+  //"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"
 });
 
 export default new OpenAIApi(configuration);
